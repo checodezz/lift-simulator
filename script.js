689c993248694dc4c475e47fe749e3a221063119
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-08-21 11:08:34 +0530

    commit

--- script.js
+++ script.js
@@ -1,135 +1,274 @@
+const userForm = document.getElementById("user_form");
+const resetBtn = document.querySelector(".reset-btn");
+const backBtn = document.querySelector(".back_btn");
+const building = document.querySelector(".building");
 
-const userForm = document.querySelector("#userForm");
-const floorsInput = document.querySelector("#floors")
-const liftsInput = document.querySelector("#lifts")
-const building = document.querySelector("#building")
+/*
+{
+  liftId : 1,
+  currentFloor : 2,
+  isOccupied : true | false,
+}
+*/
+let totalLifts = 0;
+let totalFloors = 0;
+let allLifts = [];
+let pendingFloors = [];
 
+userForm.addEventListener("submit", validateUserForm);
 
-let totalFloors = 0
-let totalLifts = 0
-userForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+resetBtn.addEventListener("click", resetAll);
 
-    const [floorCount, liftCount] = [parseInt(floorsInput.value), parseInt(liftsInput.value)];
+backBtn.addEventListener("click", () => {
+    userForm.style.display = "flex";
+    backBtn.style.display = "none";
+    resetAll();
+});
 
+function resetAll() {
+    building.innerHTML = ""; // clearing the building
+    allLifts = [];
+    pendingFloors = [];
+    totalFloors = 0;
+    totalLifts = 0;
+    userForm.reset();
+}
+
+function validateUserForm(event) {
+    event.preventDefault();
+    const formData = new FormData(userForm);
+    const [floorCount, liftCount] = [
+        Number(formData.get("totalFloors")),
+        Number(formData.get("totalLifts")),
+    ];
     if (liftCount <= 0) {
-        alert("Number of lifts should be greater than 0");
+        alert("No.of lifts should greater than 0");
     } else if (floorCount <= 0) {
-        alert("Number of floors should be greater than 0");
+        alert("No. of Floors should be greater than 0");
+    } else if (liftCount > 5) {
+        alert("Total lifts should be less than 6");
+    } else if (floorCount > 9999) {
+        alert("App will crash if the no.of floors is more than 9999");
     } else if (liftCount > floorCount) {
         alert("No. of lifts should be lesser than or equal to No. of Floors");
     } else {
+        building.innerHTML = ""; // clearing the building
         totalFloors = floorCount;
         totalLifts = liftCount;
 
-        // Call the function to generate the simulation
-        // generateSimulation(totalFloors, totalLifts);
-        generateFloors()
-    }
+        userForm.style.display = "none";
+        backBtn.style.display = "flex";
 
-    console.log(floorCount);
-    console.log(liftCount);
-});
-
-
-/* 
-function generateSimulation(floors, lifts) {
-    const simulationSection = document.querySelector("#simulation");
-    simulationSection.innerHTML = "";
-
-
-    for (let i = floors; i >= 1; i--) {
-        const floor = document.createElement("div");
-        floor.classList.add("floor");
-        floor.innerHTML = `
-        <div class="floor-buttons">
-<button class="call-lift" data-floor="${i}">Call Lift</button>
-        </div>
-        <div class="lift-shaft"></div>
-        `;
-        simulationSection.appendChild(floor);
-    };
-
-    //for lifts
-
-    const firstFloorLiftShaft = document.querySelector(".floor .lift-shaft");
-    for (let j = 0; j < lifts; j++) {
-        const lift = document.createElement('div');
-        lift.classList.add("lift");
-        lift.dataset.currentFloor = 1;
-        firstFloorLiftShaft.appendChild(lift)
+        //Generate building
+        generateFloors();
+        generateLifts();
     }
 }
 
-document.addEventListener("click", (e) => {
-    if (e.target.classList.contains("call-lift")) {
-        const targetFloor = parseInt(e.target.dataset.floor);
-        moveLiftToFloor(targetFloor);
-    }
-});
+async function buttonClickHandler(event) {
+    const element = event.target;
+    const destinationFloor = Number(element.getAttribute("floor-id"));
 
-function moveLiftToFloor(targetFloor) {
-    const lifts = document.querySelectorAll(".lift");
-    let closestLift = null;
-    let minDistance = Infinity;
-
-    // Find the closest lift to the target floor
-    lifts.forEach(lift => {
-        const currentFloor = parseInt(lift.dataset.currentFloor);
-        const distance = Math.abs(currentFloor - targetFloor);
-        if (distance < minDistance) {
-            minDistance = distance;
-            closestLift = lift;
+    // For evaluating only one lift per floor
+    const { liftElement, liftId } = checkIsLiftAlreadyPresent(destinationFloor);
+    if (liftElement) {
+        const liftState = getLiftById(liftId);
+        if (!liftState?.isOccupied) {
+            updateLiftState(liftElement, liftState.currentFloor, true);
+            await handleDoors(liftElement, getLiftById(liftId), destinationFloor);
         }
-    });
+        return;
+    }
 
-    if (closestLift) {
-        // Move the lift to the target floor
-        closestLift.style.transform = `translateY(-${(targetFloor - 1) * 100}px)`;
-        closestLift.dataset.currentFloor = targetFloor;
+    if (
+        pendingFloors.length > 0 &&
+        pendingFloors[pendingFloors.length - 1] === destinationFloor
+    ) {
+        return;
+    }
+
+    pendingFloors.push(destinationFloor);
+
+    if (isLiftAvailable()) {
+        await findLifts();
     }
 }
- */
 
+function checkIsLiftAlreadyPresent(destinationFloor) {
+    const allLiftElements = document.querySelectorAll(".lift");
+    const height = -(destinationFloor - 1) * 10;
+    for (const lift of allLiftElements) {
+        console.log(lift.style.transform == `translateY(${height}rem)`);
+        if (lift.style.transform == `translateY(${height}rem)`) {
+            let liftName = lift.id;
+            let liftId = Number(liftName.replace(/\D/g, ""));
+            return { liftElement: lift, liftId };
+        }
+    }
 
-const btnClickHandler = async (e) => {
-    const element = e.target;
-    console.log(element);
+    return { liftElement: null, liftId: null };
 }
-
 
 function generateFloors() {
     for (let i = totalFloors; i >= 1; i--) {
         const floorDiv = document.createElement("div");
-        const btnDiv = document.createElement("div");
-        const upButton = document.createElement("button");
-        const downButton = document.createElement("button");
+        const btnsDiv = document.createElement("div");
+        const upBtn = document.createElement("button");
+        const downBtn = document.createElement("button");
 
-        floorDiv.className = `floor-container`;
-        btnDiv.className = `floor-btn-container`;
+        floorDiv.className = `floor_container`;
+        btnsDiv.className = `floor_btns-container`;
 
+        upBtn.className = `btn`;
+        upBtn.textContent = "UP";
 
-        upButton.className = `btn`;
-        upButton.textContent = "Up";
+        downBtn.className = `btn`;
+        downBtn.textContent = "DOWN";
 
-        downButton.classList = `btn`;
-        downButton.textContent = "Down";
+        upBtn.addEventListener("click", buttonClickHandler);
+        downBtn.addEventListener("click", buttonClickHandler);
 
-        upButton.addEventListener("click", btnClickHandler)
-        downButton.addEventListener("click", btnClickHandler)
-
-        //edge case
-        // if (i === totalFloors) {
-        //     upButton
-        // }
+        // Edge Cases
+        if (i === totalFloors) {
+            upBtn.setAttribute("disabled", true);
+        } else if (i === 1) {
+            downBtn.setAttribute("disabled", true);
+        }
 
         floorDiv.setAttribute("floor-id", i);
-        upButton.setAttribute("floor-id", i);
-        downButton.setAttribute("floor-id", i);
+        upBtn.setAttribute("floor-id", i);
+        downBtn.setAttribute("floor-id", i);
 
-
-        btnDiv.append(upButton, downButton);
-        floorDiv.append(btnDiv);
-        building.append(floorDiv)
+        btnsDiv.append(upBtn, downBtn);
+        floorDiv.append(btnsDiv);
+        building.append(floorDiv);
     }
+}
+
+function generateLifts() {
+    const firstFloor = document.querySelector('[floor-id="1"] ');
+
+    allLifts = [];
+
+    for (let i = 1; i <= totalLifts; i++) {
+        let liftContainer = document.createElement("div");
+        liftContainer.className = "lift";
+
+        const liftDoors = document.createElement("div");
+        liftDoors.className = "lift_doors-container";
+        const liftLeftDoor = document.createElement("div");
+        const liftRightDoor = document.createElement("div");
+
+        liftLeftDoor.className = "left-door";
+        liftRightDoor.className = "right-door";
+
+        liftContainer.id = `lift ${i}`;
+
+        liftDoors.append(liftLeftDoor, liftRightDoor);
+        liftContainer.append(liftDoors);
+
+        firstFloor.append(liftContainer);
+
+        allLifts.push({ liftId: i, currentFloor: 1, isOccupied: false });
+    }
+}
+
+async function findLifts() {
+    if (pendingFloors.length > 0) {
+        let destinationFloor = pendingFloors.shift();
+        const { liftDiv, liftId } = checkIsLiftAlreadyPresent(destinationFloor);
+        if (liftDiv) {
+            const liftState = getLiftById(liftId);
+            if (!liftState?.isOccupied) {
+                updateLiftState(liftElement, liftState.currentFloor, true);
+                await handleDoors(liftElement, getLiftById(liftId), destinationFloor);
+            }
+            return;
+        }
+
+        if (isLiftAvailable()) {
+            const emptyLifts = getAllEmptyLifts();
+            const nearestLift = findNearestLift(emptyLifts, destinationFloor);
+            const liftElement = document.getElementById(`lift ${nearestLift.liftId}`);
+
+            if (nearestLift.currentFloor === destinationFloor) {
+                // same floor just open the door
+                //updating lift => occupied
+                updateLiftState(nearestLift, nearestLift.currentFloor, true);
+                await handleDoors(liftElement, nearestLift, destinationFloor);
+            } else {
+                await moveLift(liftElement, nearestLift, destinationFloor);
+            }
+        }
+    }
+}
+
+function isLiftAvailable() {
+    for (let i = 0; i < totalLifts; i++) {
+        if (!allLifts[i].isOccupied) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function getAllEmptyLifts() {
+    return allLifts.filter((lift) => !lift?.isOccupied);
+}
+
+function getLiftById(liftId) {
+    for (let i = 0; i < allLifts.length; i++) {
+        if (allLifts[i].liftId === liftId) {
+            return allLifts[i];
+        }
+    }
+}
+
+function findNearestLift(allEmptyLifts, destinationFloor) {
+    const nearestLifts = allEmptyLifts?.sort(
+        (a, b) =>
+            Math.abs(a.currentFloor - destinationFloor) -
+            Math.abs(b.currentFloor - destinationFloor)
+    );
+    return nearestLifts[0];
+}
+
+function updateLiftState(lift, currentFloor, isOccupied) {
+    lift.isOccupied = isOccupied;
+    lift.currentFloor = currentFloor;
+}
+
+async function moveLift(liftElement, lift, destinationFloor) {
+    const diffFloors = Math.abs(lift.currentFloor - destinationFloor);
+    //updating lift => occupied
+    updateLiftState(lift, lift.currentFloor, (isOccupied = true));
+
+    //moving lift
+    liftElement.style.transform = `translateY(-${(destinationFloor - 1) * 10
+        }rem)`;
+    liftElement.style.transition = `all ${diffFloors * 2}s ease`;
+    await setDelay(2 * diffFloors);
+
+    //Handling door opening
+    await handleDoors(liftElement, lift, destinationFloor);
+}
+
+async function handleDoors(liftElement, lift, destinationFloor) {
+    const doorsContainer = liftElement.firstChild;
+    doorsContainer.classList.add("openLift");
+    doorsContainer.classList.remove("closeLift");
+    await setDelay(2.5);
+    doorsContainer.classList.add("closeLift");
+    doorsContainer.classList.remove("openLift");
+    await setDelay(2.5);
+    //update lift => not occupied
+    updateLiftState(lift, destinationFloor, (isOccupied = false));
+
+    if (pendingFloors.length > 0) {
+        findLifts();
+    }
+}
+
+function setDelay(secs) {
+    return new Promise((resolve) => setTimeout(resolve, secs * 1000));
 }
